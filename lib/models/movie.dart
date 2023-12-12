8053733f653b09772a5cd62c51cd52386530b119
Sarose10 <sarojvk53@gmail.com>
2023-12-12 16:19:18 +0545

    changs

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -38,3 +38,59 @@ class Movie {
 
 
 }
+
+
+
+final data = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {"lat": "-37.3159", "lng": "81.1496"}
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+};
+
+
+class MainData{
+  final int id;
+  final Address address;
+
+  MainData({
+    required this.address,
+    required this.id
+  });
+
+}
+
+class Address{
+  final String street;
+  final Geo geo;
+  Address({
+    required this.geo,
+    required this.street
+  });
+
+}
+
+class Geo{
+  final String lat;
+  final String lng;
+  Geo({
+    required this.lat,
+    required this.lng
+  });
+
+}
+
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vk/provider/movie_provider.dart';
 
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, ref) {
+    final state = ref.watch(movieProvider);
+
+    return Scaffold(
+        body: SafeArea(
+          child: state.when(
+              data: (data){
+                return GridView.builder(
+                    itemCount: data.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3
+                    ),
+                    itemBuilder: (context, index){
+                      return Text(data[index].title);
+                    }
+                );
+              },
+              error: (err, st){
+                return Center(child: Text('$err'));
+              },
+              loading: (){
+                return Center(child: CircularProgressIndicator());
+              }
+          ),
+        )
+    );
   }
 }
