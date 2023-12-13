47177e22a4764ec34eb3edffead542e44c085ad9
Sarose10 <sarojvk53@gmail.com>
2023-12-13 15:25:00 +0545

    meal change

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:vk/provider/food_provider.dart';
 import 'package:vk/provider/movie_provider.dart';
+import 'package:vk/views/detail_page.dart';
 
 
 
@@ -9,20 +12,41 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final state = ref.watch(movieProvider);
+    final state = ref.watch(categoryProvider);
 
     return Scaffold(
         body: SafeArea(
           child: state.when(
               data: (data){
-                return GridView.builder(
-                    itemCount: data.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3
-                    ),
-                    itemBuilder: (context, index){
-                      return Text(data[index].title);
-                    }
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                      itemCount: data.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisExtent: 200
+                      ),
+                      itemBuilder: (context, index){
+                        final food = data[index];
+                        return InkWell(
+                          onTap: (){
+                            Get.to(() => DetailPage(mealCata: food.strCategory), transition: Transition.leftToRight);
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(food.strCategory),
+                                  Image.network(food.strCategoryThumb),
+                                  Text(food.strCategoryDescription, maxLines: 3,),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                  ),
                 );
               },
               error: (err, st){
